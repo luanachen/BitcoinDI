a91c96d0143a07edea85e07452739b2e7b218dc8
luanachen <luanachen@gmail.com>
2018-07-04 20:47:47 -0300

    Added swinject pod
    Signed-off-by: luanachen <luanachen@gmail.com>

---
+++ Pods/Target Support Files/Pods-BitcoinDI/Pods-BitcoinDI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitcoinDI : NSObject
+@end
+@implementation PodsDummy_Pods_BitcoinDI
+@end
---
+++ Pods/Target Support Files/Pods-BitcoinDIUITests/Pods-BitcoinDIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitcoinDIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BitcoinDIUITests
+@end
